996573d16934a8ed5d276864b212e13905513286
“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” <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>
2023-03-19 00:00:21 +0530

    files uploaded

---
+++ a1/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+import qrcode
+from io import BytesIO
+from django.core.files import File
+from PIL import Image, ImageDraw
+from django.core.validators import MaxValueValidator, MinValueValidator
+from phone_field import PhoneField
+
+
+
+# Create your models here.
+
+
+class Student(models.Model):
+    # Personal info
+    student_image = models.ImageField(upload_to="pictures")
+    qr_code = models.ImageField(upload_to='qr_code', blank=True)
+    name = models.CharField(max_length=20)
+    personal_email = models.EmailField()
+    phone =models.IntegerField()
+    gender = models.CharField(max_length=6)
+    dob = models.DateField()
+    current_address = models.CharField(max_length=100)
+    permanent_address = models.CharField(max_length=100)
+
+    # acadmeics info
+    college_name = models.CharField(max_length=100)
+    enrollment_year = models.IntegerField()
+    course = models.CharField(max_length=20)
+    branch = models.CharField(max_length=20, default="")
+    roll_no = models.IntegerField()
+    college_email = models.EmailField()
+    current_cgpa = models.IntegerField()
+    backlog = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def save(self, *args, **kwargs):
+        qr_name = "127.0.0.1:8000/student/"+str(self.roll_no)
+        qrcode_img = qrcode.make(qr_name)
+        canvas = Image.new('RGB', (350, 350), 'white')
+        draw = ImageDraw.Draw(canvas)
+        canvas.paste(qrcode_img)
+        file_name = f'qr_code-{self.roll_no}.png'
+        buffer = BytesIO()
+        canvas.save(buffer, 'PNG')
+        self.qr_code.save(file_name, File(buffer), save=False)
+        canvas.close()
+        super().save(*args, **kwargs)
